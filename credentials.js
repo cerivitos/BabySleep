03276ad01cc1514d79bcc9bd90540425968f870d
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-07-22 00:01:00 +0800

    Include into repo

---
+++ credentials.js
@@ -0,0 +1,10 @@
+export const credentials = {
+  //For production deployment on Now
+  CLIENT_ID: process.env.CLIENT_ID,
+  API_KEY: process.env.API_KEY,
+  SCOPES: process.env.SCOPES,
+  DISCOVERY_DOCS: [process.env.DISCOVERY_DOCS],
+  SPREADSHEET_ID: process.env.SPREADSHEET_ID,
+  SHEET_NAME: process.env.SHEET_NAME,
+  SHEET_ID: parseInt(process.SHEET_ID)
+};
