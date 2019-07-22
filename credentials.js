c50a56b293ae743c4ebcddaaaeb1cedbace2c3f3
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-07-22 11:20:06 +0800

    Switch to rollup replace plugin

--- credentials.js
+++ credentials.js
@@ -1,10 +1,10 @@
 export const credentials = {
   //For production deployment on Now
-  CLIENT_ID: process.env.CLIENT_ID,
-  API_KEY: process.env.API_KEY,
-  SCOPES: process.env.SCOPES,
-  DISCOVERY_DOCS: [process.env.DISCOVERY_DOCS],
-  SPREADSHEET_ID: process.env.SPREADSHEET_ID,
-  SHEET_NAME: process.env.SHEET_NAME,
-  SHEET_ID: parseInt(process.env.SHEET_ID)
+  CLIENT_ID: clientID,
+  API_KEY: apiKey,
+  SCOPES: scopes,
+  DISCOVERY_DOCS: [discoveryDocs],
+  SPREADSHEET_ID: spreadsheetID,
+  SHEET_NAME: sheetName,
+  SHEET_ID: parseInt(sheetID)
 };
--- rollup.config.js
+++ rollup.config.js
@@ -9,9 +9,12 @@ import svelte_preprocess_postcss from "svelte-preprocess-postcss";
 import builtins from "rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
 import copy from "rollup-plugin-copy-assets";
-import dotenvPlugin from "rollup-plugin-dotenv";
+import replace from "rollup-plugin-replace";
+import dotenv from "dotenv";
 
+dotenv.config();
 const production = !process.env.ROLLUP_WATCH;
+
 export default {
   input: "src/main.js",
   output: {
@@ -22,7 +25,17 @@ export default {
   },
 
   plugins: [
-    dotenvPlugin(),
+    //dotenvPlugin(),
+    replace({
+      include: "credentials.js",
+      clientID: JSON.stringify(process.env.CLIENT_ID),
+      apiKey: JSON.stringify(process.env.API_KEY),
+      scopes: JSON.stringify(process.env.SCOPES),
+      discoveryDocs: JSON.stringify(process.env.DISCOVERY_DOCS),
+      spreadsheetID: JSON.stringify(process.env.SPREADSHEET_ID),
+      sheetName: JSON.stringify(process.env.SHEET_NAME),
+      sheetID: JSON.stringify(process.env.SHEET_ID)
+    }),
     svelte({
       dev: !production,
       preprocess: {
