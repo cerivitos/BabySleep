5cd1ff10a0a02308d2439d380be203925f287285
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-07-24 10:19:29 +0800

    Upload credentials

---
+++ credentials.js
@@ -0,0 +1,15 @@
+export const credentials = {
+  CLIENT_ID:
+    "634914729787-buqfp7mh76bjh7bghe50tja5dlojkta8.apps.googleusercontent.com",
+  API_KEY: "AIzaSyCRxUfwuIvlpcqDE6uuzqxw1EUiXFL3BAQ",
+  SCOPES: "https://www.googleapis.com/auth/spreadsheets",
+  DISCOVERY_DOCS: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+  SPREADSHEET_ID: "1wTqR5OVkk-AdSLegxtkRku-JHWbXMm5Efp9azdNg_6w",
+  SHEET_NAME: "Data",
+  SHEET_ID: 1718761813
+
+  // Test worksheet
+  // SPREADSHEET_ID: "1XcXToo9F79vB-YoooATQhAY8Kb57uSpEEWdMa8gJSRw",
+  // SHEET_NAME: "Sheet1",
+  // SHEET_ID: 0
+};
