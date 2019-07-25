8b699179be5179af11a5a0968591bbe6f54c96cc
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-07-26 00:42:52 +0800

    Create scatter plot graph

--- src/components/Summary.svelte
+++ src/components/Summary.svelte
@@ -5,61 +5,163 @@
   import LoadingSpinner from "./LoadingSpinner.svelte";
   import { credentials } from "../../credentials.js";
   import { isSameDay, format } from "date-fns";
+  import Chart from "chart.js";
 
   const historicalRows = 20;
   let loading = false;
   let getTodayData;
 
   let todayDatas = [];
+  let historicalDatas = [];
 
-  onMount(() => {
-    if ($gapiInstance !== undefined) {
-      loading = true;
+  function getData() {
+    loading = true;
 
-      $gapiInstance.client.sheets.spreadsheets.values
-        .get({
-          spreadsheetId: credentials.SPREADSHEET_ID,
-          range: credentials.SHEET_NAME + "!A1:A"
-        })
-        .then(response => {
-          const lastRow = response.result.values.length;
-          const firstRow = lastRow - historicalRows + 1;
+    $gapiInstance.client.sheets.spreadsheets.values
+      .get({
+        spreadsheetId: credentials.SPREADSHEET_ID,
+        range: credentials.SHEET_NAME + "!A1:A"
+      })
+      .then(response => {
+        const lastRow = response.result.values.length;
+        const firstRow = lastRow - historicalRows + 1;
 
-          getTodayData = $gapiInstance.client.sheets.spreadsheets.values
-            .get({
-              spreadsheetId: credentials.SPREADSHEET_ID,
-              range: credentials.SHEET_NAME + `!A${firstRow}:Q${lastRow}`
-            })
-            .then(response => {
-              loading = false;
-              console.log(response);
-              const sheetData = response.result.values;
-              /**
-               * Need to add year to the data from Sheets as it is received as a string
-               */
-              const year = format(new Date(), "yyyy");
+        getTodayData = $gapiInstance.client.sheets.spreadsheets.values
+          .get({
+            spreadsheetId: credentials.SPREADSHEET_ID,
+            range: credentials.SHEET_NAME + `!A${firstRow}:Q${lastRow}`
+          })
+          .then(response => {
+            loading = false;
+            console.log(response);
+            const sheetData = response.result.values;
 
-              for (let i = sheetData.length - 1; i > 15; i--) {
-                const date = new Date(sheetData[i][0].replace(",", ` ${year}`));
-                todayDatas.push(sheetData[i]);
-              }
+            historicalDatas = sheetData.reverse();
 
-              // for (let i = sheetData.length - 1; i > 0; i--) {
-              //   const date = new Date(sheetData[i][0].replace(",", ` ${year}`));
+            /**
+             * Need to add year to the data from Sheets as it is received as a string
+             */
+            const year = format(new Date(), "yyyy");
 
-              //   if (isSameDay(date, new Date())) {
-              //     todayDatas.push(sheetData[i]);
-              //   } else {
-              //     break;
-              //   }
-              // }
+            for (let i = sheetData.length - 1; i > 15; i--) {
+              const date = new Date(sheetData[i][0].replace(",", ` ${year}`));
+              todayDatas.push(sheetData[i]);
+            }
 
-              todayDatas.reverse();
-              console.log(todayDatas);
-            });
-        });
+            // for (let i = sheetData.length - 1; i > 0; i--) {
+            //   const date = new Date(sheetData[i][0].replace(",", ` ${year}`));
+
+            //   if (isSameDay(date, new Date())) {
+            //     todayDatas.push(sheetData[i]);
+            //   } else {
+            //     break;
+            //   }
+            // }
+
+            todayDatas.reverse();
+            console.log(todayDatas);
+          });
+      });
+  }
+
+  function plotPutDownVsTimeToFallAsleep() {
+    const ctx = document.getElementById("putDownVsTimeToFallAsleep");
+    let scatterChartData = [];
+
+    for (let i = 0; i < historicalDatas.length; i++) {
+      const pair = {
+        x:
+          parseInt(historicalDatas[i][5].split(":")[0] * 60) +
+          parseInt(historicalDatas[i][5].split(":")[1]),
+        y:
+          parseInt(historicalDatas[i][7].split(":")[0] * 60) +
+          parseInt(historicalDatas[i][7].split(":")[1])
+      };
+      scatterChartData.push(pair);
     }
-  });
+
+    console.log(scatterChartData);
+
+    Chart.defaults.scale.gridLines.display = false;
+
+    let chart = new Chart(ctx, {
+      type: "scatter",
+      data: {
+        datasets: [
+          {
+            label: "Put Down vs. Time to Fall Asleep",
+            data: scatterChartData,
+            pointBackgroundColor: "#2EC4B6"
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 1,
+        tooltips: {
+          callbacks: {
+            label: (tooltipItem, data) => {
+              return (
+                "Time to Fall Asleep: " +
+                tooltipItem.label +
+                " Put Down: " +
+                tooltipItem.value
+              );
+            }
+          }
+        },
+        title: {
+          display: true,
+          fontColor: "#8D99AE",
+          text: "Put Down vs. Time to Fall Asleep",
+          fontSize: 16,
+          padding: 16
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Time to Fall Asleep (min)",
+                fontColor: "#EDF2F4"
+              },
+              ticks: {
+                fontColor: "#EDF2F4"
+              },
+              gridLines: {
+                color: "#8D99AE"
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Put Down (min)",
+                fontColor: "#EDF2F4"
+              },
+              ticks: {
+                fontColor: "#EDF2F4"
+              },
+              gridLines: {
+                color: "#8D99AE"
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+
+  $: if ($gapiInstance !== undefined) {
+    getData();
+  }
+
+  $: if (historicalDatas.length > 0) {
+    plotPutDownVsTimeToFallAsleep();
+  }
 </script>
 
 <style type="text/postcss">
@@ -71,62 +173,58 @@
     @apply text-primaryColor;
   }
 
-  h3 {
-    @apply text-primaryColor m-0;
-  }
-
-  th,
   td {
     @apply px-2 py-1;
     text-align: center;
     vertical-align: center;
   }
 
-  .category {
-    @apply rounded-full uppercase bg-accentColor2 text-secondaryColor text-sm font-medium mt-4 mb-2 py-1 px-2 shadow;
-    width: fit-content;
-  }
-
-  .cell {
-    @apply text-secondaryColor;
+  th {
+    @apply text-primaryColor font-bold p-2;
   }
 </style>
 
 <div class="w-full bg-backgroundColor p-4">
-  <div class="mt-8">
+  <div>
     <h2>Today</h2>
     {#await getTodayData}
       <LoadingSpinner />
     {:then}
-      {#each todayDatas as todayData}
-        <div class="category">{todayData[11]} {todayData[12]}</div>
-        <table class="w-full">
-          <thead>
-            <tr>
-              <th>
-                <h3>Put down</h3>
-              </th>
-              <th>
-                <h3>Fell asleep</h3>
-              </th>
-              <th>
-                <h3>Woke up</h3>
-              </th>
-              <th>
-                <h3>Picked up</h3>
-              </th>
-            </tr>
-          </thead>
+      <table class="w-full">
+        <thead>
+          <tr>
+            <th>
+              <p>Put down</p>
+            </th>
+            <th>
+              <p>Fell asleep</p>
+            </th>
+            <th>
+              <p>Woke up</p>
+            </th>
+            <th>
+              <p>Picked up</p>
+            </th>
+          </tr>
+        </thead>
+        {#each todayDatas as todayData}
+          <h3 class="text-sm text-accentColor3">
+            {todayData[11]} {todayData[12]}
+          </h3>
           <tbody>
-            <tr class="cell">
+            <tr class="text-secondaryColor">
               <td>{todayData[0].split(', ')[1].toLowerCase()}</td>
               <td>{todayData[1].split(', ')[1].toLowerCase()}</td>
               <td>{todayData[2].split(', ')[1].toLowerCase()}</td>
               <td>{todayData[3].split(', ')[1].toLowerCase()}</td>
             </tr>
           </tbody>
-        </table>
-      {/each}
+        {/each}
+      </table>
     {/await}
+  </div>
+  <div class="mt-8">
+    <h2>Trends</h2>
+    <canvas id="putDownVsTimeToFallAsleep" class="w-full h-48" />
   </div>
 </div>
