63bed21a3829460e49111e014e30044c40515362
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-07-27 00:06:06 +0800

    Add duration convert fns

--- src/util.js
+++ src/util.js
@@ -34,3 +34,14 @@ export function signOut() {
       userPic.set();
     });
 }
+
+/**
+ * Converts a duration in the form "hh:mm:ss" into minutes
+ * @param {string} duration A string in the format "hh:mm:ss" representing a duration
+ * @returns {number} The duration converted to minutes
+ */
+export function convertToMins(duration) {
+  return (
+    parseInt(duration.split(":")[0]) * 60 + parseInt(duration.split(":")[1])
+  );
+}
